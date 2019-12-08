341e27f750da9ec09a06822fdbb8159325b4092b
Ardhi <ardhifnoor@outlook.com>
2019-12-08 14:39:37 +0700

    Run app on iOS device

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, SafeAreaView } from 'react-native'
 
 import Header from './src/components/Header'
 
 export default class App extends Component {
   render(){
     return(
-      <View>
-        <Header title="Authentication"/>
-      </View>
+      <SafeAreaView>
+        <View>
+          <Header title="Authentication"/>
+        </View>
+      </SafeAreaView>
     )
   }
 }
